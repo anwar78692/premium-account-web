4ecc699e09412cdd75f8f7f0c95d73dd44190818
anwar78692 <anwar15298@gmail.com>
2024-08-01 00:16:58 +0530

    delete styled component file

--- pages/components/MainPage/Header/TopHeader.js
+++ pages/components/MainPage/Header/TopHeader.js
@@ -1,6 +1,16 @@
-import { StyledDivTop, StyledH1 } from '@/pages/styledcomponents/StyledComponent'
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
+export const StyledH1 = styled.h1`
+  color: white;
+  font-size: 24px;
+  text-align: center;
+`;
+
+const StyledDivTop = styled.div`
+background-color: red;
+padding: 6px;
+`
 function Header() {
     return (
         <StyledDivTop>
