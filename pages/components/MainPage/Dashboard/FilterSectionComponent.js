829be30d0da34945e3ea95988e6cd7e1878cc3b5
anwarahmad <anwar15298@gmail.com>
2024-08-03 03:41:20 +0530

    delete accordian

--- pages/components/MainPage/Dashboard/FilterSectionComponent.js
+++ pages/components/MainPage/Dashboard/FilterSectionComponent.js
@@ -13,7 +13,22 @@ import styled from "styled-components";
 import RememberMe from "@/pages/customcomponent/Checkbox";
 // const OptionSelector = dynamic(() => import('@/pages/customcomponent/OptionSelector'), { ssr: false });
 
+import React from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+const AccordionItem = ({ expanded, handleChange, panel, title, subtitle, content }) => (
+    <Accordion expanded={expanded} onChange={handleChange(panel)} className='font-bold'>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${panel}-content`} id={`${panel}-header`}>
+        <Typography className='font-bold' sx={{ width: '100%', flexShrink: 0 }}>{title}</Typography>
+        <Typography sx={{ color: 'text.secondary' }}>{subtitle}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>{content}</Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+  
 const OptionSelectorContainer = styled.div`
   .header {
     display: flex;
