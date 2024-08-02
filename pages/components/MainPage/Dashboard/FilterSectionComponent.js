7a182b9a866366c02e83bcf763a14c68eb255efb
anwarahmad <anwar15298@gmail.com>
2024-08-03 03:31:46 +0530

    add in same file

--- pages/components/MainPage/Dashboard/FilterSectionComponent.js
+++ pages/components/MainPage/Dashboard/FilterSectionComponent.js
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import { Button } from "@mui/material";
 import React, { useState } from 'react';
 import FilterButtons from './FilterButtons';
@@ -9,7 +9,61 @@ import AccordionItem from '@/pages/customcomponent/Accordian';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 import StoreMallDirectoryIcon from '@mui/icons-material/StoreMallDirectory';
-const OptionSelector = dynamic(() => import('@/pages/customcomponent/OptionSelector'), { ssr: false });
+import styled from "styled-components";
+import RememberMe from "@/pages/customcomponent/Checkbox";
+// const OptionSelector = dynamic(() => import('@/pages/customcomponent/OptionSelector'), { ssr: false });
+
+
+const OptionSelectorContainer = styled.div`
+  .header {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 16px 7px 0px;
+    border-bottom: 2px solid #ccc;
+    margin-top: 15px;
+    // border-top: 2px solid #ccc;
+  }
+
+  .options {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    margin-right: 10px;
+  }
+
+  .option {
+    flex: 1 1 calc(50% - 10px);
+  }
+`;
+
+function OptionSelector({ title, icon: Icon, options, selectedOption, setSelectedOption }) {
+
+
+  const handleOptionChange = (option) => () => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <OptionSelectorContainer>
+      <div className="header">
+        <h1 className="font-bold text-xl">{title}</h1>
+        <Icon />
+      </div>
+      <div className="options">
+        {options.map((option, index) => (
+          <div key={index} className="option">
+            <RememberMe
+              label={option}
+              isChecked={selectedOption === option}
+              onChange={handleOptionChange(option)}
+            />
+          </div>
+        ))}
+      </div>
+    </OptionSelectorContainer>
+  );
+}
+
 
  const accordionItems = [
     {
