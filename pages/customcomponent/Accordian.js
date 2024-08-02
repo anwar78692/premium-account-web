c2e49a6240e3d34fe9fb61ad7d15ff9642c60b60
anwarahmad <anwar15298@gmail.com>
2024-08-03 03:37:06 +0530

    taccordian

--- pages/customcomponent/Accordian.js
+++ pages/customcomponent/Accordian.js
@@ -3,7 +3,7 @@ import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const AccordionItem = ({ expanded, handleChange, panel, title, subtitle, content }) => (
-    <Accordion expanded={expanded} onChange={() =>handleChange(panel)} className='font-bold'>
+    <Accordion expanded={expanded} onChange={handleChange(panel)} className='font-bold'>
       <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${panel}-content`} id={`${panel}-header`}>
         <Typography className='font-bold' sx={{ width: '100%', flexShrink: 0 }}>{title}</Typography>
         <Typography sx={{ color: 'text.secondary' }}>{subtitle}</Typography>
